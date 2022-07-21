94949afd7c2b26d3024dc60a98736438e1e5a9bf
Daniel Mason <danmasson0@gmail.com>
2022-07-20 22:46:09 -0400

    implement navbar, and hero portions of the project along with respective css stylings

--- src/Hero.js
+++ src/Hero.js
@@ -1,8 +1,27 @@
-import React from 'react'
-import phoneImg from './images/phone.svg'
-
+import React from "react";
+import phoneImg from "./images/phone.svg";
+import { useGlobalContext } from "./context";
 const Hero = () => {
-  return <h2>hero component</h2>
-}
+  const { closeSubmenu } = useGlobalContext();
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <article className="hero-info">
+          <h1>Payment infrastructure for the internet</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
+            quod, neque deleniti nihil fugiat aliquid odit. Quisquam labore
+            neque magni et eos dolor, itaque maxime ipsam, harum sequi natus
+            culpa.
+          </p>
+          <button className="btn">Start now</button>
+        </article>
+        <article className="hero-images">
+          <img src={phoneImg} className="phone-img" alt="phone" />
+        </article>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
